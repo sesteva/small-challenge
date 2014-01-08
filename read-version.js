513095da2f9e671093d3a3531625e404e0a3c7e7
labrador <labrador@labradoodle.com>
2014-01-08 17:00:28 -0600

    added readme

--- read-version.js
+++ read-version.js
@@ -4,11 +4,11 @@ var fs = require('fs'),
 var stream = byline(fs.createReadStream('/etc/lsb-release', { encoding: 'utf8' }));
 
 stream.on('data', function(line) {
-    console.log(line);
+//    Uncomment To prove only first lines where read
+//    console.log(line);
     if(line.match(/DISTRIB_CODENAME/)){
-        console.log(line);
+        console.log(line.split('=')[1]);
         stream.pause();
     }
-
 });
 
